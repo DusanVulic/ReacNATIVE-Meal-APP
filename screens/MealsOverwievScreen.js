39aff014aa4d239578be2a86e26c6d3fc0762a48
Dusan <dusanvulic11@gmail.com>
2022-06-14 12:53:05 +0200

    meal items list

--- screens/MealsOverwievScreen.js
+++ screens/MealsOverwievScreen.js
@@ -1,13 +1,30 @@
 import { MEALS } from "../data/dummy-data";
 
-import { View, Text, StyleSheet } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
+//import component
+import MealItem from "../components/MealItem";
 
 const MealsOverwievScreen = ({ route }) => {
   const catId = route.params.categoryId;
 
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  const renderMealItem = (itemData) => {
+    return (
+      <View>
+        <MealItem title={itemData.item.title} />
+      </View>
+    );
+  };
   return (
     <View style={styles.container}>
-      <Text>MealsOverwievScreen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
