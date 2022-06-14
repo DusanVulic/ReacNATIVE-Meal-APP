7408062d17150c22350f21efe3fe6527b1a9ac5c
Dusan <dusanvulic11@gmail.com>
2022-06-14 13:08:30 +0200

    adding images and other props for single meal

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,11 +1,33 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Pressable, StyleSheet, Image } from "react-native";
 
-const MealItem = ({ title }) => {
+const MealItem = ({ title, imageUrl, duration, complexity, affordability }) => {
   return (
     <View>
-      <Text>{title}</Text>
+      <Pressable>
+        <View>
+          <Image source={{ uri: imageUrl }} style={styles.image} />
+          <Text style={styles.title}>{title}</Text>
+        </View>
+        <View>
+          <Text> {duration} min</Text>
+          <Text>{complexity.toUpperCase()}</Text>
+          <Text> {affordability.toUpperCase()}</Text>
+        </View>
+      </Pressable>
     </View>
   );
 };
 
 export default MealItem;
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+    textAlign: "center",
+  },
+});
--- screens/MealsOverwievScreen.js
+++ screens/MealsOverwievScreen.js
@@ -14,7 +14,13 @@ const MealsOverwievScreen = ({ route }) => {
   const renderMealItem = (itemData) => {
     return (
       <View>
-        <MealItem title={itemData.item.title} />
+        <MealItem
+          title={itemData.item.title}
+          imageUrl={itemData.item.imageUrl}
+          duration={itemData.item.duration}
+          complexity={itemData.item.complexity}
+          affordability={itemData.item.affordability}
+        />
       </View>
     );
   };
